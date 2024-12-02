418697919cf969a744dbb2c594fbf181181027db
shoma-mano <ms2geki@gmail.com>
2024-12-03 02:56:05 +0900

    working

--- books/0/packages/nuxt3/src/builder/template.ts
+++ books/0/packages/nuxt3/src/builder/template.ts
@@ -1,12 +1,8 @@
-import { join, relative, dirname } from 'path'
-import { get } from 'http'
+import { join, dirname } from 'path'
 import fsExtra from 'fs-extra'
-import globby from 'globby'
-import lodashTemplate from 'lodash/template'
 import * as nxt from './nxt'
 
 export interface NuxtTemplate {
-  src?: string // Absolute path to source file
   fileName: string // Relative path of destination
   getContents?: (data: {
     globals: any;
@@ -26,7 +22,7 @@ export function templateData (builder) {
   }
 }
 
-async function compileTemplate ({ src, fileName, data, getContents }: NuxtTemplate, destDir: string) {
+async function compileTemplate ({ fileName, data, getContents }: NuxtTemplate, destDir: string) {
   const compiledSrc = getContents ? getContents(data) : ''
   const dest = join(destDir, fileName)
   await fsExtra.mkdirp(dirname(dest))
@@ -38,16 +34,6 @@ export function compileTemplates (templates: NuxtTemplate[], destDir: string) {
     return compileTemplate(t, destDir)
   }))
 }
-
-// export async function scanTemplates (dir: string, data?: Object) {
-//   const templateFiles = (await globby(join(dir, '/**')))
-
-//   return templateFiles.map(src => ({
-//     src,
-//     path: relative(dir, src),
-//     data
-//   }))
-// }
 
 export function watchTemplate (template: NuxtTemplate, _watcher: any, _cb: Function) {
   template.data = new Proxy(template.data, {
