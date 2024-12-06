5c297b14e730ca2815a27fc96d8d70c52a21f699
shoma-mano <ms2geki@gmail.com>
2024-12-06 18:50:26 +0900

    middlewareを削除中

--- books/0/packages/nitro/src/runtime/server/index.ts
+++ books/0/packages/nitro/src/runtime/server/index.ts
@@ -1,24 +1,27 @@
-import '../app/config'
-import { createApp, useBase } from 'h3'
-import { createFetch } from 'ohmyfetch'
-import destr from 'destr'
-import { createCall, createFetch as createLocalFetch } from '@nuxt/un/runtime/fetch'
-import { timingMiddleware } from './timing'
-import { handleError } from './error'
+import "../app/config";
+import { createApp, useBase } from "h3";
+import { createFetch } from "ohmyfetch";
+import destr from "destr";
+import {
+  createCall,
+  createFetch as createLocalFetch,
+} from "@nuxt/un/runtime/fetch";
+import { handleError } from "./error";
 // @ts-ignore
-import serverMiddleware from '~serverMiddleware'
+import serverMiddleware from "~serverMiddleware";
 
 const app = createApp({
   debug: destr(process.env.DEBUG),
-  onError: handleError
-})
+  onError: handleError,
+});
 
-app.use(timingMiddleware)
-app.use(serverMiddleware)
-app.use(() => import('../app/render').then(e => e.renderMiddleware), { lazy: true })
+app.use(serverMiddleware);
+app.use(() => import("../app/render").then((e) => e.renderMiddleware), {
+  lazy: true,
+});
 
-export const stack = app.stack
-export const handle = useBase(process.env.ROUTER_BASE, app)
-export const localCall = createCall(handle)
-export const localFetch = createLocalFetch(localCall, global.fetch)
-export const $fetch = global.$fetch = createFetch({ fetch: localFetch })
+export const stack = app.stack;
+export const handle = useBase(process.env.ROUTER_BASE, app);
+export const localCall = createCall(handle);
+export const localFetch = createLocalFetch(localCall, global.fetch);
+export const $fetch = (global.$fetch = createFetch({ fetch: localFetch }));
