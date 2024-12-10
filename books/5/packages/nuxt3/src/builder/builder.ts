4955456a280d3de68d935c355649c275a300e9fb
shoma-mano <ms2geki@gmail.com>
2024-12-11 01:38:50 +0900

    5をもっと削ぎ落としていく

--- books/5/packages/nuxt3/src/builder/builder.ts
+++ books/5/packages/nuxt3/src/builder/builder.ts
@@ -1,33 +1,9 @@
-import { relative } from "path";
 import fsExtra from "fs-extra";
 import { Nuxt } from "../core";
-import { DeterminedGlobals, determineGlobals } from "../utils";
 import { templateData, compileTemplates, NuxtTemplate } from "./template";
-import { createApp, NuxtApp } from "./app";
-import Ignore from "./ignore";
+import { createApp } from "./app";
 import * as defaultTemplates from "./templates";
 
-// export class Builder {
-//   nuxt: Nuxt;
-//   globals: DeterminedGlobals;
-//   ignore: Ignore;
-//   templates: NuxtTemplate[];
-//   app: NuxtApp;
-
-//   constructor(nuxt) {
-//     this.nuxt = nuxt;
-//   }
-
-//   build() {
-//     return build(this);
-//   }
-
-//   close() {
-//     // TODO: close watchers
-//   }
-// }
-
-// Extends VueRouter
 export async function build(nuxt: Nuxt) {
   await fsExtra.emptyDir(nuxt.options.buildDir);
   await generate(nuxt);
@@ -39,8 +15,6 @@ export async function build(nuxt: Nuxt) {
 
 export async function generate(nuxt: Nuxt) {
   const app = await createApp(nuxt);
-  // Todo: Call app:created hook
-
   const data = templateData(nuxt, app);
   const templates = Object.values(defaultTemplates).map((t) => ({
     ...t,
