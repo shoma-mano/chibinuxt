b8b639171948f9c491b4f515678caba84d1f53ba
shoma-mano <ms2geki@gmail.com>
2026-01-08 11:46:43 +0900

    feat(nitro): refactor appDistDir handling to use a centralized export

--- impls/5-packages/packages/nuxt/src/core/nuxt.ts
+++ impls/5-packages/packages/nuxt/src/core/nuxt.ts
@@ -2,10 +2,9 @@ import { join, resolve } from 'node:path'
 import { createDevServer, createNitro } from 'nitro'
 import { bundle } from '@nuxt/vite-builder'
 
+export const appDistDir = join(import.meta.dirname, '../../dist/app')
 export const loadNuxt = async () => {
   // this is temporary way
-  const appDistDir = join(import.meta.dirname, '../../dist/app')
-  process.env.APP_DIST_DIR = appDistDir
   await bundle({
     appDistDir,
     clientEntry: join(import.meta.dirname, '../app/entry.client.ts'),
